dc6dc08fbc0db51748441872e9c6a3b92cbedd42
TaynaraPereira <102699339+TaynaraPereira@users.noreply.github.com>
2022-04-08 11:32:42 -0400

    Add files via upload

---
+++ ProjetoTeste/Calculadora.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetoTeste
+{
+    /// <summary>
+    /// Lógica interna para Calculadora.xaml
+    /// </summary>
+    public partial class Calculadora : Window
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btAdicao_Click(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(txtNum1.Text);
+
+            double num2 = Convert.ToDouble(txtNum2.Text);
+
+            double resultado = num1 + num2;
+            txtResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void btMultiplicar_Click(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(txtNum1.Text);
+
+            double num2 = Convert.ToDouble(txtNum2.Text);
+
+            double resultado = num1 * num2;
+            txtResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void btDividir_Click(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(txtNum1.Text);
+
+            double num2 = Convert.ToDouble(txtNum2.Text);
+
+            double resultado = num1 / num2;
+            txtResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void btSubtracao_Click(object sender, RoutedEventArgs e)
+        {
+            double num1 = Convert.ToDouble(txtNum1.Text);
+
+            double num2 = Convert.ToDouble(txtNum2.Text);
+
+            double resultado = num1 - num2;
+            txtResultado.Text = Convert.ToString(resultado);
+        }
+
+        private void txtResultado_TextChanged()
+        {
+
+        }
+    }
+}
---
+++ ProjetoTeste/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjetoTeste
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btAcessar_Click(object sender, RoutedEventArgs e)
+        {
+
+            string senha = txtSenha.Password.ToString();
+            string user = txtUsuario.Text;
+
+
+            if (user == "thaypereira69021@gmail.com" & senha == "1234")
+            {
+
+                MessageBox.Show("Usuário e senha corretos!");
+                txtUsuario.Clear();
+                txtSenha.Clear();
+                Calculadora calculadora = new Calculadora();
+                calculadora.Show();
+                this.Close();
+            }
+            else
+            {
+
+                MessageBox.Show("Usuário ou senha incorreto!");
+                txtUsuario.Clear();
+                txtSenha.Clear();
+            }
+        }
+    }
+}
